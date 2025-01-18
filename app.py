256ec8e207631ccfeff7672837809d4cf6cacab3
Martin Svardsjo <martincsvardsjo@gmail.com>
2025-01-18 15:20:31 +0000

    Updated style of site

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000,ssl_context=('cert.pem', 'key.pem'))
+    app.run(host='0.0.0.0', port=5000, ssl_context = ('cert.pem', 'key.pem'))
